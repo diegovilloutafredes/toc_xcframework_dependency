dd12cca3b664bb9baf66db659ff9fcbfc7457255
Diego <diego.villouta@sovos.com>
2023-09-15 05:13:36 -0300

    New version

--- README.md
+++
@@ -1,1 +0,0 @@
-# toc_xcframework_dependency
---
+++ SocketIO.xcframework/ios-arm64_x86_64-simulator/SocketIO.framework/Headers/SocketIO.h
@@ -0,0 +1,16 @@
+//
+//  SocketIO.h
+//  SocketIO
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SocketIO.
+FOUNDATION_EXPORT double SocketIOVersionNumber;
+
+//! Project version string for SocketIO.
+FOUNDATION_EXPORT const unsigned char SocketIOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SocketIO/PublicHeader.h>
+
+
